936c935f93fc674df35d30ecda2c39ceb8e7f53b
JohnMa17 <johnelmenorgv15@gmail.com>
2024-08-15 23:36:39 -0400

    Agregar archivos de proyecto.

---
+++ numerosprimos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace numerosprimos
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (primo(numero))
+            {
+                Console.WriteLine("El número es primo.");
+            }
+            else
+            {
+                Console.WriteLine("El número no es primo.");
+            }
+        }
+
+        static bool primo(int numero)
+        {
+            if (numero <= 1) return false;
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
